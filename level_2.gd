da936454f6a30150a8b3fb9985b9826c7466855e
Andres Carballo <andres.carballo@gmail.com>
2023-10-14 22:36:18 +0200

    se añade puerta
    puerta transicion entre pantallas

--- level_2.gd
+++ level_2.gd
@@ -3,19 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("level_1 listo")
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	# print(event);
 	if event.is_action_pressed("volver_menu_principal"):
 		get_tree().change_scene_to_file("res://main.tscn")
-	
-
 
 func _on_door_body_entered(body):
-	print_debug("entrando puerta")
 	if body == $Player:
-		get_tree().change_scene_to_file("res://level_2.tscn")
+		get_tree().change_scene_to_file("res://level_1.tscn")
